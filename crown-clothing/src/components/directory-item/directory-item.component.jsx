bb1e581eccaedbf798febcad5a27762bb2f4b086
Oliver <214707419@qq.com>
2024-06-26 18:12:23 -0400

    fixed minor issues

--- crown-clothing/src/components/directory-item/directory-item.component.jsx
+++ crown-clothing/src/components/directory-item/directory-item.component.jsx
@@ -1,15 +1,22 @@
 
 import {DirectoryContainer,BackgroundImage,Body} from './directory-item.styles.jsx';
+import { useNavigate } from 'react-router-dom';
 
 const DirectoryItem = ({category}) => {
-    const {title,imageUrl} = category
+    const {title,imageUrl,route} = category
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => {
+      navigate(route);
+    }
+
     return (
-        <DirectoryContainer>
-                <BackgroundImage imageUrl={imageUrl} style={{backgroundImage: `url(${imageUrl})`}}/>
-                <Body>
-                  <h2>{title}</h2>
-                  <p>Shop now</p>
-                </Body>
+        <DirectoryContainer onClick={onNavigateHandler}>
+          <BackgroundImage imageUrl={imageUrl}/>
+          <Body>
+            <h2>{title}</h2>
+            <p>Shop now</p>
+          </Body>
         </DirectoryContainer>
     )
 }
