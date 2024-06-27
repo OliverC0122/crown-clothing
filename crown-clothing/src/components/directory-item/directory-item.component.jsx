653eda09e4a9900e1a0e14e89a3320f61ad86ab6
Oliver <214707419@qq.com>
2024-06-27 09:47:44 -0400

    minor change in the code

--- crown-clothing/src/components/directory-item/directory-item.component.jsx
+++ crown-clothing/src/components/directory-item/directory-item.component.jsx
@@ -12,7 +12,7 @@ const DirectoryItem = ({category}) => {
 
     return (
         <DirectoryContainer onClick={onNavigateHandler}>
-          <BackgroundImage imageUrl={imageUrl}/>
+          <BackgroundImage imageurl={imageUrl}/>
           <Body>
             <h2>{title}</h2>
             <p>Shop now</p>
