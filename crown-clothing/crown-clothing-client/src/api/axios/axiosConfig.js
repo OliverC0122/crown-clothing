03e046192594779172896a8a1a9821a4b2af48a8
Oliver <214707419@qq.com>
2024-09-16 17:34:13 -0400

    fixing api url

--- crown-clothing/crown-clothing-client/src/api/axios/axiosConfig.js
+++ crown-clothing/crown-clothing-client/src/api/axios/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:8888",
+    baseURL: "https://crown-clothing-back-282e48e6fbc7.herokuapp.com/",
     "Content-type": "application/json"
 });
